353f3c375cf09aad5e8e32516ffad864ab798c1f
AndreyChim <chimitcyrenov@mail.ru>
2025-01-01 16:36:12 +0800

    Add copper color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    "./src/**/*.{html,js}"
   ],
   theme: {
     extend: {
@@ -18,5 +19,27 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
+  ],
+  colors: {
+    copper: {
+      100: "#FAD9C1",
+      200: "#F6C8A4",
+      300: "#F2B786",
+      400: "#EEA669",
+      500: "#E9944C",
+      600: "#D17F3E",
+      700: "#B96A31",
+      800: "#A15524",
+      900: "#8A4018",
+      dark: "#8A4018",
+    },
+  },
+  fontFamily: {
+    serif: ["Times", "serif"],
+  },
+  extend: {
+    spacing: {
+      "8xl": "108rem",
+    },
+  },
 }
