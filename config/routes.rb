4214fb5b3b109132cca73f80ee2836a64d8e4d86
AndreyChim <chimitcyrenov@mail.ru>
2025-01-17 14:31:30 +0800

    Generate controller Course

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'courses#index'
+  get "courses/index"
   resources :users
   get "landing/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
