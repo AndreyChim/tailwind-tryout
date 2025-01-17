3f61d0d9f42e87666fc8a1acac6ea0414e2afb73
AndreyChim <chimitcyrenov@mail.ru>
2025-01-17 12:09:57 +0800

    Fix colors configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
+    colors: {
+      copper: {
+        100: "#FAD9C1",
+        200: "#F6C8A4",
+        300: "#F2B786",
+        400: "#EEA669",
+        500: "#E9944C",
+        600: "#D17F3E",
+        700: "#B96A31",
+        800: "#A15524",
+        900: "#8A4018",
+        dark: "#8A4018",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
@@ -16,24 +30,10 @@ module.exports = {
     },
   },
   plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ],
-  colors: {
-    copper: {
-      100: "#FAD9C1",
-      200: "#F6C8A4",
-      300: "#F2B786",
-      400: "#EEA669",
-      500: "#E9944C",
-      600: "#D17F3E",
-      700: "#B96A31",
-      800: "#A15524",
-      900: "#8A4018",
-      dark: "#8A4018",
-    },
-  },
   fontFamily: {
     serif: ["Times", "serif"],
   },
