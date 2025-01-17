f61e6cae4087ba4e735dceeadff14d22d1f93aa6
AndreyChim <chimitcyrenov@mail.ru>
2025-01-17 16:00:44 +0800

    Include helper to component Enrollee

--- app/components/enrollee_component.rb
+++ app/components/enrollee_component.rb
@@ -1,4 +1,7 @@
-# frozen_string_literal: true
-
 class EnrolleeComponent < ViewComponent::Base
+  include CoursesHelper
+    
+    def total_enrollees(course)
+      count_enrollees(course)
+    end
 end
