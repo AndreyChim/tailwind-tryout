141b010f78e577024cd8a664574f02d09193ca9e
AndreyChim <chimitcyrenov@mail.ru>
2025-01-17 16:27:13 +0800

    Add method before_render

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,6 +1,10 @@
 module CoursesHelper
-    def count_enrollees(course)
-      count = (course.price / 4).round()
-      tag.p "#{count} enrollees so far"
-    end
+  def count_enrollees(course)
+    count = (course.price / 4).round()
+    tag.p "#{count} enrollees so far"
+  end
+
+  def star_icon
+    image_tag("/assets/star.png", width: "1%")
+  end
 end
