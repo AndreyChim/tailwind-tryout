f74346ade81515bd630bf9be0c51a2a18f01fd04
AndreyChim <chimitcyrenov@mail.ru>
2025-01-19 14:16:27 +0800

    Add assert to discount component

--- test/components/discount_component_test.rb
+++ test/components/discount_component_test.rb
@@ -5,5 +5,7 @@ class DiscountComponentTest < ViewComponent::TestCase
   def test_render_component
     course = Course.create(title: 'Organizing your Time', price: 55.00, location: 'London')
     render_inline(DiscountComponent.new(item: course))
+    assert_selector 'p[class="green"]'
+    assert_text "10% discount"
   end
 end
