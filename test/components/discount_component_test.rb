137ef3288e002806695f75463231db26bbbe7582
AndreyChim <chimitcyrenov@mail.ru>
2025-01-19 13:50:00 +0800

    Add test component discount

--- test/components/discount_component_test.rb
+++ test/components/discount_component_test.rb
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
-
 require "test_helper"
+require "view_component/test_case"
 
 class DiscountComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(DiscountComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_render_component
+    course = Course.create(title: 'Organizing your Time', price: 55.00, location: 'London')
+    render_inline(DiscountComponent.new(item: course))
   end
 end
