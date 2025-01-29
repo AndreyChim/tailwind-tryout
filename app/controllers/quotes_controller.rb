0dadc55d10f56be664f4f03ef5a783dd9ae860f6
AndreyChim <chimitcyrenov@mail.ru>
2025-01-29 18:49:33 +0800

    Try requests with xhr & without

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,26 +1,46 @@
 class QuotesController < ApplicationController
-  def index
-    @quotes = Quote.all
-  end
-
-  def show
-    @quote = Quote.find(params[:id])
-  end
 
   def new
     @quote = Quote.new
   end
 
-  def create
-    # @quote = Quote.new(body: params[:body])
+  def index
+    @quotes = Quote.all
+  end
 
-    # respond_to do |format|
-    # format.html { redirect_to quotes_path }
+  def create
+    @quote = Quote.new(quotes_params)
+    respond_to do |format|
+      if @quote.save
+        format.html { redirect_to quotes_path, notice: "Quote created successfully." }
+        format.js   # This will look for a `create.js.erb` file
+      else
+        format.html { render :new }
+        format.js   # This will look for a `create.js.erb` file
+      end
+    end
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+  end
     
-    # end
+
+  def update
+    @quote = Quote.find(params[:id])
+    if @quote.update(quotes_params)
+      redirect_to @quote, notice: "User updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def quotes_params
+    params.require(:quote).permit(:name)
   end
 end
-
 
 
 
