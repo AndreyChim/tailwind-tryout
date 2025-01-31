8614efb905b8c916d7e6ef237587f2930e642c79
AndreyChim <chimitcyrenov@mail.ru>
2025-01-31 16:06:34 +0800

    Add page controller & view

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,9 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { Application } from "@hotwired/stimulus";
+import HelloController from "./hello_controller";
+
+const application = Application.start();
+application.register("hello", HelloController);
 eagerLoadControllersFrom("controllers", application)
