aa7b25d66adbd501e20e0cc9248c7e063a18d7d2
AndreyChim <chimitcyrenov@mail.ru>
2025-02-04 10:23:27 +0800

    Type some element's value

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,16 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = [ "name" ]
+
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+    console.log("Hello, Stimulus!", this.element)
+  }
+  
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`hello, ${name}!`)
   }
 }
